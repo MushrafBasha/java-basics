5b9b6bb53750ef5e509550fe340094eca85103dc
MushrafBasha <bashamushraf01@gmail.com>
2024-07-01 09:50:50 +0530

    methods

--- src/main/java/intern/methods.java
+++ src/main/java/intern/methods.java
@@ -9,16 +9,17 @@ public class methods {
     public void intern2(String name,int age ){
         System.out.println("name :"+name+"\nage:"+age);
     }
-    public void intern3(){
-        System.out.println("with return");
-
+    public char intern3(){
+        System.out.println("with return type and without arguments");
+        return 'M';
     }
 
     public static void main(String[]args){
         methods obj=new methods();
         obj.intern();
         obj.intern2("mushraf",20);
-
+        char gender = obj.intern3();
+        System.out.println("current person name(female,male)"+gender);
     }
 
 }
